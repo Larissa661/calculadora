257de7a271bdaf804e998117ec720f272d0833eb
Larissa661 <alexandrelarissa04@gmail.com>
2026-05-26 20:49:03 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -44,6 +44,14 @@ function receberPorcentagem(evento) {
         botao.classList.add("botao-ativo")
     }
 })
+if(evento.target.value !== ""){
 
 porcentagem = parseFloat(evento.target.value) / 100
+} else {
+    porcentagem = 0 
 }
+
+console.log(porcentagem)
+}
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
